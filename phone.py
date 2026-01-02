ab657d794de5625af3682529f6fd300216561235
Umair12122 <un399067@gmail.com>
2026-01-02 20:35:21 +0000

    First python project - calculator/ learning logical operators

---
+++ phone.py
@@ -0,0 +1,16 @@
+class Phone:
+
+    def __init__(self, brand, price):
+        self.brand = brand
+        self.price = price
+
+    def call(self, Phone_Number):
+        print(f"{self.brand} is calling {PhoneNumber}")
+
+iphone = Phone ("Iphone 7+", 300)
+Samsung= Phone ("Samsung S20", 1400)
+
+print(iphone.brand)
+print(iphone.price)
+print(Samsung.brand)
+print(Samsung.price)
